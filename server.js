c43d9af12739334d693c4aed67d04a30c0cf7174
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-18 15:32:54 +0200

    create userSchema

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/nodeappdatabase', {
     useMongoClient: true
 });
+
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+    name: String,
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    admin: Boolean
+});
